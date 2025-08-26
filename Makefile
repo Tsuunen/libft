961df97f38f8552ece10f2ae2aedfa170c3f9b35
Remi LAFORGUE <laforgue.remi@gmail.com>
2025-08-26 18:05:03 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,27 @@
+CC = cc
+CFLAGS = -Werror -Wextra -Wall
+NAME = libft.a
+INCLUDES = libft.h
+OBJS = $(SRCS:.c=.o)
+SRCS = ft_isalpha.c\
+	   ft_isdigit.c\
+	   ft_isalnum.c\
+	   ft_isascii.c
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $^
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ -c $^
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: relaforg <relaforg@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 16:45:04 by relaforg          #+#    #+#             */
+/*   Updated: 2025/08/24 17:08:31 by relaforg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+
+#endif
